e3a5f8c1e31290850d10933b2f186283eac48bf3
Matthew Rimar Martus <181802171+Mr-cmd-pip@users.noreply.github.com>
2025-10-24 20:23:00 +0800

    feat: added my backend dependencies

---
+++ OAuth Integration/backend/oauth2.integration/src/test/java/it342/g02/martus/matthewrimar/oauth2/integration/ApplicationTests.java
@@ -0,0 +1,13 @@
+package it342.g02.martus.matthewrimar.oauth2.integration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
