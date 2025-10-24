1c345ba7adc3d5cc2355e7dd839baefd4d25f198
Matthew Rimar Martus <matthewrimar1102@gmail.com>
2025-10-24 21:34:57 +0800

    feat: updated my repository file and added my code

--- OAuth-Integration/backend/oauth2.integration/src/main/java/it342/g02/martus/matthewrimar/oauth2/integration/repository/AuthProviderRepository.java
+++ OAuth-Integration/backend/oauth2.integration/src/main/java/it342/g02/martus/matthewrimar/oauth2/integration/repository/AuthProviderRepository.java
@@ -1,5 +1,13 @@
 package it342.g02.martus.matthewrimar.oauth2.integration.repository;
 
-public class AuthProviderRepository {
-    
+import it342.g02.martus.matthewrimar.oauth2.integration.entity.AuthProvider;
+import it342.g02.martus.matthewrimar.oauth2.integration.entity.ProviderType;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface AuthProviderRepository extends JpaRepository {
+    Optional findByProviderAndProviderUserId(ProviderType provider, String providerUserId);
 }
--- OAuth-Integration/backend/oauth2.integration/src/main/java/it342/g02/martus/matthewrimar/oauth2/integration/repository/UserRepository.java
+++ OAuth-Integration/backend/oauth2.integration/src/main/java/it342/g02/martus/matthewrimar/oauth2/integration/repository/UserRepository.java
@@ -1,5 +1,13 @@
 package it342.g02.martus.matthewrimar.oauth2.integration.repository;
 
-public class UserRepository {
-    
+import it342.g02.martus.matthewrimar.oauth2.integration.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository {
+    Optional findByEmail(String email);
 }
+
